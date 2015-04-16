612f4e09626b16859a0c1af9df3907fbb880ee50
peterhn <peterhn2@uci.edu>
2015-04-15 20:07:02 -0700

    ignore obj and bin

--- AutismAppJam/Controllers/MBTITestController.cs
+++ AutismAppJam/Controllers/MBTITestController.cs
@@ -19,7 +19,40 @@ namespace AutismAppJam.Controllers
         [HttpPost]
         public ActionResult Index(List<string> question)
         {
+            int eSum, iSum, sSum, nSum, tSum, fSum, jSum, pSum;
+            eSum = iSum = sSum = nSum = tSum = fSum = jSum = pSum = 0;
 
+            foreach (string s in question)
+            {
+                if (s == "E1")
+                    eSum++;
+                if (s == "I1")
+                    iSum++;
+                if (s == "S1")
+                    sSum++;
+                if (s == "S2") 
+                    sSum++;
+                if (s == "N2") 
+                    nSum++;
+                if (s == "T1")
+                    tSum++;
+                if (s == "F1")
+                    fSum++;
+                if (s == "T2") 
+                    tSum++;
+                if (s == "F2")
+                    fSum++;
+                if (s == "J1") 
+                    jSum++;
+                if (s == "P1") 
+                    pSum++;
+                if (s == "J2") 
+                    jSum++;
+                if (s == "P2") 
+                    pSum++;
+
+
+            }
             return View();
         }
     }
