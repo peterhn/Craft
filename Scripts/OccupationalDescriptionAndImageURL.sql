2f58229e652ef91a156e97112e795cbdc7276cb5
Brandon Lim <bdlim2004@gmail.com>
2015-04-19 13:37:31 -0700

    Occupational Trend SQL Scripts
    - SQL Script to fix Database Inconsistencies
    - SQL Script to add Occupational Description and Image URLS

---
+++ Scripts/OccupationalDescriptionAndImageURL.sql
@@ -0,0 +1,114 @@
+﻿UPDATE OccupationalTrends
+SET OccupationDescription = 'Responsible for managing a single unit, different sectors, or multiple units of a company or organization. Hires and trains employees, prepares reports, and sets budgets. Managers usually set tools and objectives for department or unit. Managers may also hire, train, and terminate workers as needed.',
+OccupationImageURL = '../Content/images/occupation/manager.jpg'
+WHERE Occupation = 'Management';
+
+UPDATE OccupationalTrends
+SET OccupationDescription = 'A administrator provides both clerical and administrative support to professionals, either as part of a team or individually. The role plays a vital part in the administration and smooth-running of businesses throughout industry. Administrators are involved with the coordination and implementation of office procedures and frequently have responsibility for specific projects and tasks and, in some cases, oversee and supervise the work of junior staff.',
+OccupationImageURL = '../Content/images/occupation/administrator.jpg'
+WHERE Occupation = 'Administration';
+
+UPDATE OccupationalTrends
+SET OccupationDescription = 'The duties of a a law enforcement officer focus on protecting people and property. They patrol the areas they are assigned, which sometimes include entire jurisdictions, respond to calls, enforce laws, make arrests, issue citations, and occasionally testify in court cases. They often make traffic stops, respond to domestic disturbances, and, at times, provide first aid to someone involved in a traffic accident or injured in a domestic dispute until paramedics arrive. Much of their time is divided between patrolling, writing reports, and filling out forms.',
+OccupationImageURL = '../Content/images/occupation/lawenforcement.jpg'
+WHERE Occupation = 'Law Enforcement';
+
+UPDATE OccupationalTrends
+SET OccupationDescription = 'Prepares and analyzes financial reports, including balance sheets, income and loss statements, and tax returns. Accountants ensure accurate and appropriate recording and analysis of revenues and expenses. Also, accountants resolve accounting discrepancies and irregularities. They also ensure accurate and appropriate recording and analysis of revenues and expenses.',
+OccupationImageURL = '../Content/images/occupation/accountant.jpg'
+WHERE Occupation = 'Accounting';
+
+UPDATE OccupationalTrends
+SET OccupationDescription = 'Educators develop schemes of work and lesson plans in line with curriculum objectives. They facilitate learning by establishing a relationship with pupils, and by their organization of learning resources and the classroom learning environment.',
+OccupationImageURL = '../Content/images/occupation/educator.jpg'
+WHERE Occupation = 'Education';
+
+UPDATE OccupationalTrends
+SET OccupationDescription = "Responsible for caring for sick, injured, or disabled people in a healthcare setting. Works under the direction of a registered nurse. Examples of responsibilities includes: care for people who are sick, injured, convalescent, or disabled, providing bedside care, or recording patients' vital signs such as height, weight, temperature, blood pressure, pulse, and respiration",
+OccupationImageURL = '../Content/images/occupation/healthcare.jpg'
+WHERE Occupation = 'Health Care';
+
+UPDATE OccupationalTrends
+SET OccupationDescription = 'Religious providers are responsible for leading worship, and providing guidance and instruction to members of their faith, sect or tribe. They are also responsible for any associated administrative duties. Religious services covers a range of titles and functions such as Aboriginal ceremonial celebrant, chaplain, imam, minister, missionary, monk, nun, pastor, priest, rabbi, religious brother/sister, religion teacher, religious counsellor and religious youth leader.',
+OccupationImageURL = '../Content/images/occupation/religion.jpg'
+WHERE Occupation = 'Religion';
+
+UPDATE OccupationalTrends
+SET OccupationDescription = 'Counselors help people to explore feelings and emotions that are often related to their experiences. This allows their clients to reflect on what is happening to them and consider alternative ways of doing things. Counselors work in a confidential setting and listen attentively to their clients. They offer them the time, empathy and respect they need to express their feelings and perhaps understand themselves from a different perspective. The aim is reduce their confusion and enable them to cope with challenges or to make positive changes in their life where necessary.',
+OccupationImageURL = '../Content/images/occupation/counselor.jpg'
+WHERE Occupation = 'Counseling';
+
+UPDATE OccupationalTrends
+SET OccupationDescription = 'A teacher job will vary according to the school, the grade and the level of the job. However most school teacher jobs contain elements of each of these typical responsibilities including teaching, student management, administrative tasks and extracurricular activities. Teachers generally plan, organize and implement an appropriate instructional program in a learning environment that guides and encourages students to develop and fulfill their academic potential.',
+OccupationImageURL = '../Content/images/occupation/teacher.jpg'
+WHERE Occupation = 'Teaching';
+
+UPDATE OccupationalTrends
+SET OccupationDescription = 'To put it simply, an artist is a person who creates art for either sale or exhibition. Artists use many different techniques and materials in their work, and they can be classified as either craft artists or fine artists depending on the type of work that they create.',
+OccupationImageURL = '../Content/images/occupation/artist.jpg'
+WHERE Occupation = 'Arts';
+
+UPDATE OccupationalTrends
+SET OccupationDescription = 'Research scientists are responsible for designing, undertaking and analyzing information from controlled laboratory-based investigations, experiments and trials.Their work is almost entirely laboratory-based, with responsibilities that include conducting experiments, recording and analyzing data, and writing research papers, reports, reviews and summaries.',
+OccupationImageURL = '../Content/images/occupation/scientist.jpg'
+WHERE Occupation = 'Science';
+
+UPDATE OccupationalTrends
+SET OccupationDescription = "A field service technician is a general term for any repair or assessment technician who travels to a site to troubleshoot equipment problems. A field service technician may work for a product manufacturer or be employed by a dedicated team of repairmen, such as Best Buy's Geek Squad, whose job is to repair and maintain all makes and models of a particular type of product.",
+OccupationImageURL = '../Content/images/occupation/technicalfield.jpg'
+WHERE Occupation = 'Technical';
+
+UPDATE OccupationalTrends
+SET OccupationDescription = 'Technology workers are generally responsible for coordinating the installation of computer software programs and networks, including computer-assisted engineering programs for schematic cabling projects, modeling programs for cellular and satellite systems, and programs for telephone options, such as voice mail, email, and call waiting.',
+OccupationImageURL = '../Content/images/occupation/technology.jpg'
+WHERE Occupation = 'Technology';
+
+UPDATE OccupationalTrends
+SET OccupationDescription = 'Computer programmers write code to create software programs. They turn the program designs created by software developers and engineers into instructions that a computer can follow. Programmers must debug the programs—that is, test them to ensure that they produce the expected results. If a program does not work correctly, they check the code for mistakes and fix them.',
+OccupationImageURL = '../Content/images/occupation/computer.jpg'
+WHERE Occupation = 'Computers';
+
+UPDATE OccupationalTrends
+SET OccupationDescription = 'Lawyers serve as both advocates and advisers. As advocates, they speak for their clients in court by presenting supportive evidence. As advisers, they counsel their clients on their legal rights and obligations. Lawyers—also called attorneys and counselors—can interpret laws, apply laws to specific situations, and draft new laws.',
+OccupationImageURL = '../Content/images/occupation/lawyer.jpg'
+WHERE Occupation = 'Law';
+
+UPDATE OccupationalTrends
+SET OccupationDescription = 'An employee in this class has general construction and maintenance responsibilities for a large facility. Duties involve performing the actual work, and in some cases, supervising unskilled or semi-skilled workers in the tasks. Work is performed under general supervision. Some responsibilities may include: welding in construction tasks, repairing and maintaining building interiors and exteriors. or building wooden structures by sheeting and rough carpentry.',
+OccupationImageURL = '../Content/images/occupation/skilledtrade.jpg'
+WHERE Occupation = 'Skilled Trades';
+
+UPDATE OccupationalTrends
+SET OccupationDescription = 'Jobs in agriculture span the spectrum from laborers picking lettuce and tomatoes to scientific researchers studying animal nutrition and crop development. Potential employers range from ranches and farms to universities and government agencies. While some positions have indoor tasks in labs and offices, many agricultural jobs involve work outdoors in fields and ranches.',
+OccupationImageURL = '../Content/images/occupation/agriculture.jpg'
+WHERE Occupation = 'Agriculture';
+
+UPDATE OccupationalTrends
+SET OccupationDescription = 'Armed forces officers work in the service of their country to defend its people and they also support international peacekeeping and humanitarian efforts across the globe. Responsibilities vary widely across the various job roles within each branch but typical activities include: planning maneuvers, assigning duties and communicating effectively with other staff, commanding, training and leading subordinate personnel, and monitoring the welfare and progress of new recruits.',
+OccupationImageURL = '../Content/images/occupation/military.jpg'
+WHERE Occupation = 'Military';
+
+UPDATE OccupationalTrends
+SET OccupationDescription = 'Businessmen and women work in a variety of fields including health care, finance, education, construction, and investment. Business careers range in level, responsibility, and experience, and have many specializations. Depending on the industry, experience and education of the individual, business people work to make sure all areas of a department are running efficiently and productively. Business people must oversee employees, delegate tasks, plan for objectives to be met, and hold responsibility for meeting financial and production goals.',
+OccupationImageURL = '../Content/images/occupation/business.jpg'
+WHERE Occupation = 'Business';
+
+UPDATE OccupationalTrends
+SET OccupationDescription = 'Writers and authors typically do the following choose subject matter that interests readers. They usually write fiction or nonfiction through scripts, novels, and biographies. Also, writers conduct research to obtain factual information and authentic detail. In other fields, writers advertising copy for use by newspapers, magazines, broadcasts, and the Internet. At work, drafts are presented to editors and clients for feedback.Working with editors and clients help shape the material so it can be published.',
+OccupationImageURL = '../Content/images/occupation/writer.jpg'
+WHERE Occupation = 'Writing';
+
+UPDATE OccupationalTrends
+SET OccupationDescription = 'Marketing careers attract creative and driven individuals. Though most marketing jobs are specialized, marketing typically draws on your ability to match perception of the market with achievable plans of action. Best for self-starters who can manage multiple tasks, a career in marketing can be demanding—long hours and travel are often required— but the field rewards merit and performance.',
+OccupationImageURL = '../Content/images/occupation/marketing.jpg'
+WHERE Occupation = 'Marketing';
+
+UPDATE OccupationalTrends
+SET OccupationDescription = 'Responsible for supervising, nurturing, and caring for children typically from ages 6 weeks to 5 years old, before the child enters kindergarten. Welcomes children each day, provides meals, performs crafts and activities, changes diapers, and ensures children behave well when in contact with other children.',
+OccupationImageURL = '../Content/images/occupation/childcare.jpg'
+WHERE Occupation = 'Child Care';
+
+UPDATE OccupationalTrends
+SET OccupationDescription = 'A team leader is someone who provides direction, instructions and guidance to a group of individuals, who can also be known as a team, for the purpose of achieving a certain goal. An effective leader will know her team members strengths, weaknesses and motivations. Team leaders serve various roles in an organization. Their job is to get tasks done by using all of the resources available to them, including other employees or team members.',
+OccupationImageURL = '../Content/images/occupation/leader.jpg'
+WHERE Occupation = 'Leadership';
---
+++ Scripts/OccupationalTrendsFix.sql
@@ -0,0 +1,14 @@
+﻿UPDATE OccupationalTrends
+SET Occupation = 'Religion'
+WHERE Occupation = 'Religious Settings';
+
+
+UPDATE OccupationalTrends
+SET Occupation = 'Science'
+WHERE Occupation = 'Scientific/Technical';
+
+INSERT INTO OccupationalTrends (Occupation, PersonalityType)
+VALUES ('Technical', 'INTJ');
+
+INSERT INTO OccupationalTrends (Occupation, PersonalityType)
+VALUES ('Technical', 'INTP');
