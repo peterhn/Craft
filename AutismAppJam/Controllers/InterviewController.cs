398e036abfeab11afdeb16db0997658270131d9e
peterhn <peterhn2@uci.edu>
2015-04-19 00:12:20 -0700

    added interviews controller and index page

---
+++ AutismAppJam/Controllers/InterviewController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AutismAppJam.Controllers
+{
+    public class InterviewController : Controller
+    {
+        //
+        // GET: /Interview/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
