77f586e02640d467818783ee039799393816983b
Francis <fraphong@gmail.com>
2015-04-19 00:57:31 -0700

    Modified Database for Profile Pic
    Profile Picture Implementation

---
+++ Scripts/PersonalitiesImageURL.sql
@@ -0,0 +1,63 @@
+﻿UPDATE Personalities
+SET PersonalityImageURL='../Content/images/ENFJ.jpg'
+WHERE PersonalityType = 'ENFJ';
+
+UPDATE Personalities
+SET PersonalityImageURL='../Content/images/ENFP.jpg'
+WHERE PersonalityType = 'ENFP';
+
+UPDATE Personalities
+SET PersonalityImageURL='../Content/images/ENTJ.jpg'
+WHERE PersonalityType = 'ENTJ';
+
+UPDATE Personalities
+SET PersonalityImageURL='../Content/images/ENTP.jpg'
+WHERE PersonalityType = 'ENTP';
+
+UPDATE Personalities
+SET PersonalityImageURL='../Content/images/ESFJ.jpg'
+WHERE PersonalityType = 'ESFJ';
+
+UPDATE Personalities
+SET PersonalityImageURL='../Content/images/ESFP.jpg'
+WHERE PersonalityType = 'ESFP';
+
+UPDATE Personalities
+SET PersonalityImageURL='../Content/images/ESTJ.jpg'
+WHERE PersonalityType = 'ESTJ';
+
+UPDATE Personalities
+SET PersonalityImageURL='../Content/images/ESTP.jpg'
+WHERE PersonalityType = 'ESTP';
+
+UPDATE Personalities
+SET PersonalityImageURL='../Content/images/INFJ.jpg'
+WHERE PersonalityType = 'INFJ';
+
+UPDATE Personalities
+SET PersonalityImageURL='../Content/images/INFP.jpg'
+WHERE PersonalityType = 'INFP';
+
+UPDATE Personalities
+SET PersonalityImageURL='../Content/images/INTJ.jpg'
+WHERE PersonalityType = 'INTJ';
+
+UPDATE Personalities
+SET PersonalityImageURL='../Content/images/INTP.jpg'
+WHERE PersonalityType = 'INTP';
+
+UPDATE Personalities
+SET PersonalityImageURL='../Content/images/ISFJ.jpg'
+WHERE PersonalityType = 'ISFJ';
+
+UPDATE Personalities
+SET PersonalityImageURL='../Content/images/ISFP.jpg'
+WHERE PersonalityType = 'ISFP';
+
+UPDATE Personalities
+SET PersonalityImageURL='../Content/images/ISTJ.jpg'
+WHERE PersonalityType = 'ISTJ';
+
+UPDATE Personalities
+SET PersonalityImageURL='../Content/images/ISTP.jpg'
+WHERE PersonalityType = 'ISTP';
