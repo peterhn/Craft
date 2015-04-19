0d22b17b229dfb630e43810e924a3bf5fca6d09d
Brandon Lim <bdlim2004@gmail.com>
2015-04-19 11:13:59 -0700

    Image Organization and Database Update
    - Reorganized Personality and Occupational Images into subfolders
    - Updated Database variables for longer URL length
    - Ran SQL scripts to populate database with new URL values

--- Scripts/PersonalitiesImageURL.sql
+++ Scripts/PersonalitiesImageURL.sql
@@ -1,63 +1,63 @@
 ﻿UPDATE Personalities
-SET PersonalityImageURL='../Content/images/ENFJ.jpg'
+SET PersonalityImageURL='../Content/images/personality/ENFJ.jpg'
 WHERE PersonalityType = 'ENFJ';
 
 UPDATE Personalities
-SET PersonalityImageURL='../Content/images/ENFP.jpg'
+SET PersonalityImageURL='../Content/images/personality/ENFP.jpg'
 WHERE PersonalityType = 'ENFP';
 
 UPDATE Personalities
-SET PersonalityImageURL='../Content/images/ENTJ.jpg'
+SET PersonalityImageURL='../Content/images/personality/ENTJ.jpg'
 WHERE PersonalityType = 'ENTJ';
 
 UPDATE Personalities
-SET PersonalityImageURL='../Content/images/ENTP.jpg'
+SET PersonalityImageURL='../Content/images/personality/ENTP.jpg'
 WHERE PersonalityType = 'ENTP';
 
 UPDATE Personalities
-SET PersonalityImageURL='../Content/images/ESFJ.jpg'
+SET PersonalityImageURL='../Content/images/personality/ESFJ.jpg'
 WHERE PersonalityType = 'ESFJ';
 
 UPDATE Personalities
-SET PersonalityImageURL='../Content/images/ESFP.jpg'
+SET PersonalityImageURL='../Content/images/personality/ESFP.jpg'
 WHERE PersonalityType = 'ESFP';
 
 UPDATE Personalities
-SET PersonalityImageURL='../Content/images/ESTJ.jpg'
+SET PersonalityImageURL='../Content/images/personality/ESTJ.jpg'
 WHERE PersonalityType = 'ESTJ';
 
 UPDATE Personalities
-SET PersonalityImageURL='../Content/images/ESTP.jpg'
+SET PersonalityImageURL='../Content/images/personality/ESTP.jpg'
 WHERE PersonalityType = 'ESTP';
 
 UPDATE Personalities
-SET PersonalityImageURL='../Content/images/INFJ.jpg'
+SET PersonalityImageURL='../Content/images/personality/INFJ.jpg'
 WHERE PersonalityType = 'INFJ';
 
 UPDATE Personalities
-SET PersonalityImageURL='../Content/images/INFP.jpg'
+SET PersonalityImageURL='../Content/images/personality/INFP.jpg'
 WHERE PersonalityType = 'INFP';
 
 UPDATE Personalities
-SET PersonalityImageURL='../Content/images/INTJ.jpg'
+SET PersonalityImageURL='../Content/images/personality/INTJ.jpg'
 WHERE PersonalityType = 'INTJ';
 
 UPDATE Personalities
-SET PersonalityImageURL='../Content/images/INTP.jpg'
+SET PersonalityImageURL='../Content/images/personality/INTP.jpg'
 WHERE PersonalityType = 'INTP';
 
 UPDATE Personalities
-SET PersonalityImageURL='../Content/images/ISFJ.jpg'
+SET PersonalityImageURL='../Content/images/personality/ISFJ.jpg'
 WHERE PersonalityType = 'ISFJ';
 
 UPDATE Personalities
-SET PersonalityImageURL='../Content/images/ISFP.jpg'
+SET PersonalityImageURL='../Content/images/personality/ISFP.jpg'
 WHERE PersonalityType = 'ISFP';
 
 UPDATE Personalities
-SET PersonalityImageURL='../Content/images/ISTJ.jpg'
+SET PersonalityImageURL='../Content/images/personality/ISTJ.jpg'
 WHERE PersonalityType = 'ISTJ';
 
 UPDATE Personalities
-SET PersonalityImageURL='../Content/images/ISTP.jpg'
+SET PersonalityImageURL='../Content/images/personality/ISTP.jpg'
 WHERE PersonalityType = 'ISTP';
