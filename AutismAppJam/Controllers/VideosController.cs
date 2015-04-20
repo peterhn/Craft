a54a8ca8a933855501266cd9edb8f7edd0892326
peterhn <peterhn2@uci.edu>
2015-04-20 16:52:45 -0700

    added new controller for videos and updated dashboard

---
+++ AutismAppJam/Controllers/VideosController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AutismAppJam.Controllers
+{
+    public class VideosController : Controller
+    {
+        public ActionResult Index()
+        {
+            
+
+            return View();
+        }
+    }
+}
