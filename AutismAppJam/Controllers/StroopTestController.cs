eaf0a9f164f405a61ebfd1cbdbafd7f22dfe5e77
peterhn <peterhn2@uci.edu>
2015-04-20 17:50:14 -0700

    add stroop test controller

---
+++ AutismAppJam/Controllers/StroopTestController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace AutismAppJam.Controllers
+{
+    public class StroopTestController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Index(List<string> questions)
+        {
+
+
+            return View();
+        }
+    }
+}
---
+++ AutismAppJam/Scripts/js/stroop.js
@@ -0,0 +1,160 @@
+
+$(document).ready(function() {
+
+	$("#stroop_question_2, #stroop_question_3, #stroop_question_4, #stroop_question_5, #stroop_question_6, #stroop_question_7, #stroop_question_8, #stroop_question_9, #stroop_question_10, #stroop_question_11, #stroop_question_12, #stroop_question_13, #stroop_question_14, #stroop_question_15, #stroop_question_16, #stroop_question_17, #stroop_question_18, #stroop_question_19, #stroop_question_20").hide();
+
+	$("#stroop_question_1_finish").click(function () {
+        $("#stroop_question_2").show();
+        $("#stroop_question_1").hide();
+
+        return false;
+
+   	});
+
+   	$("#stroop_question_2_finish").click(function () {
+        $("#stroop_question_3").show();
+        $("#stroop_question_2").hide();
+
+        return false;
+
+   	});
+
+   	$("#stroop_question_3_finish").click(function () {
+        $("#stroop_question_4").show();
+        $("#stroop_question_3").hide();
+
+        return false;
+
+   	});
+
+   	$("#stroop_question_4_finish").click(function () {
+        $("#stroop_question_5").show();
+        $("#stroop_question_4").hide();
+
+        return false;
+
+   	});
+
+   	$("#stroop_question_5_finish").click(function () {
+        $("#stroop_question_6").show();
+        $("#stroop_question_5").hide();
+
+        return false;
+
+   	});
+
+   	$("#stroop_question_6_finish").click(function () {
+        $("#stroop_question_7").show();
+        $("#stroop_question_6").hide();
+
+        return false;
+
+   	});
+
+   	$("#stroop_question_7_finish").click(function () {
+        $("#stroop_question_8").show();
+        $("#stroop_question_7").hide();
+
+        return false;
+
+   	});
+
+   	$("#stroop_question_8_finish").click(function () {
+        $("#stroop_question_9").show();
+        $("#stroop_question_8").hide();
+
+        return false;
+
+   	});
+
+   	$("#stroop_question_9_finish").click(function () {
+        $("#stroop_question_10").show();
+        $("#stroop_question_9").hide();
+
+        return false;
+
+   	});
+
+   	$("#stroop_question_10_finish").click(function () {
+        $("#stroop_question_11").show();
+        $("#stroop_question_10").hide();
+
+        return false;
+
+   	});
+
+   	$("#stroop_question_11_finish").click(function () {
+        $("#stroop_question_12").show();
+        $("#stroop_question_11").hide();
+
+        return false;
+
+   	});
+
+   	$("#stroop_question_12_finish").click(function () {
+        $("#stroop_question_13").show();
+        $("#stroop_question_12").hide();
+
+        return false;
+
+   	});
+
+   	$("#stroop_question_13_finish").click(function () {
+        $("#stroop_question_14").show();
+        $("#stroop_question_13").hide();
+
+        return false;
+
+   	});
+
+   	$("#stroop_question_14_finish").click(function () {
+        $("#stroop_question_15").show();
+        $("#stroop_question_14").hide();
+
+        return false;
+
+   	});
+
+   	$("#stroop_question_15_finish").click(function () {
+        $("#stroop_question_16").show();
+        $("#stroop_question_15").hide();
+
+        return false;
+
+   	});
+
+   	$("#stroop_question_16_finish").click(function () {
+        $("#stroop_question_17").show();
+        $("#stroop_question_16").hide();
+
+        return false;
+
+   	});
+
+   	$("#stroop_question_17_finish").click(function () {
+        $("#stroop_question_18").show();
+        $("#stroop_question_17").hide();
+
+        return false;
+
+   	});
+
+   	$("#stroop_question_18_finish").click(function () {
+        $("#stroop_question_19").show();
+        $("#stroop_question_18").hide();
+
+        return false;
+
+   	});
+
+   	$("#stroop_question_19_finish").click(function () {
+        $("#stroop_question_20").show();
+        $("#stroop_question_19").hide();
+
+        return false;
+
+   	});
+
+
+
+});
