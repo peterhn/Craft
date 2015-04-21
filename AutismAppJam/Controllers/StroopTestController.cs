831fef308a6ba8b3f269533800d540f3a9b0d765
Thanh Tran <tltran1@gmail.com>
2015-04-20 19:12:37 -0700

    Added Stroop Test CSS

--- AutismAppJam/Controllers/StroopTestController.cs
+++ AutismAppJam/Controllers/StroopTestController.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Web.Http;
 using System.Web.Mvc;
 
 namespace AutismAppJam.Controllers
 {
+
+    [Authorize]
     public class StroopTestController : Controller
     {
         public ActionResult Index()
@@ -15,6 +16,7 @@ namespace AutismAppJam.Controllers
             return View();
         }
 
+        [HttpPost]
         public ActionResult Index(List<string> questions)
         {
 
